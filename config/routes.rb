2bb31ff358f6f991745b02cc0709fa13b0fe6578
Winston  Chung <18.chung.winston@ctkprep.org>
2020-03-01 04:09:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+<<<<<<< HEAD
   get '/auth/:provider/callback', to: 'sessions#create'
   #match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+=======
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+>>>>>>> d5424109e67b338d18ccf22ea0c4f51d81192305
 end
